2a8d4837b88aa8be80f1168c4bad241f6d765e0f
luiz kieth patiag <patiagluiz27@gmail.com>
2024-09-23 18:24:22 +0800

    Updates

---
+++ precache-manifest.1039642f1d687e54e429d1d915e129e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89b8ef8f49656aa98147c8121913bc3f",
+    "url": "/luiz-ctrl/ws101-PATIAG-NAVBAR.git/index.html"
+  },
+  {
+    "revision": "19f3792a7fbe9f029c81",
+    "url": "/luiz-ctrl/ws101-PATIAG-NAVBAR.git/static/css/main.e0eab5a1.chunk.css"
+  },
+  {
+    "revision": "92e23421ff612ea251ce",
+    "url": "/luiz-ctrl/ws101-PATIAG-NAVBAR.git/static/js/2.8851f76e.chunk.js"
+  },
+  {
+    "revision": "19f3792a7fbe9f029c81",
+    "url": "/luiz-ctrl/ws101-PATIAG-NAVBAR.git/static/js/main.5d468b87.chunk.js"
+  },
+  {
+    "revision": "1cce7b7a0d843ba88ae0",
+    "url": "/luiz-ctrl/ws101-PATIAG-NAVBAR.git/static/js/runtime~main.38422b26.js"
+  }
+]);
